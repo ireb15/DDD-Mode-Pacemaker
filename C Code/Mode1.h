1b619706b04542ccaf4aee38027dec6a7e4d4286
ireb15 <57650854+ireb15@users.noreply.github.com>
2020-10-14 18:56:57 +1300

    Added header file for SCCharts code

---
+++ C Code/Mode1.h
@@ -0,0 +1,16 @@
+#ifndef MODE1_H_ 
+#define MODE1_H_ 
+
+// Functions
+void reset(); 
+void tick(); 
+
+// Inputs
+extern char VSense; 
+extern char ASense;  
+
+// Outputs
+extern char VPace;
+extern char APace; 
+
+#endif/* MODE1_H_ */
