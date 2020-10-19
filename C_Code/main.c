a4bfb584313b3ae48c1e0685180b49e7aa464330
Harrison Warahi <hwar042@aucklanduni.ac.nz>
2020-10-19 13:41:17 +1300

    Update main.c

--- C_Code/main.c
+++ C_Code/main.c
@@ -6,15 +6,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include "sys/alt_alarm.h"
-#include "Mode1.c"
-#include "Mode1.h"
 
-// Example timeout values in milliseconds 
-#define AVI_VALUE 300 
-#define AEI_VALUE 800 
-#define PVARP_VALUE 50 
-#define VRP_VALUE 150 
-#define LRI_VALUE 950 
+#include "mode1.c"
+#include "mode1.h"
+
+// Example timeout values in milliseconds
+#define AVI_VALUE 300
+#define AEI_VALUE 800
+#define PVARP_VALUE 50
+#define VRP_VALUE 150
+#define LRI_VALUE 950
 #define URI_VALUE 900
 
 #define ESC 27
@@ -29,7 +30,6 @@
 //Global Variables
 int currentMode = 0;
 int prevMode = 0;
-int ticks_per_second = alt_ticks_per_second();
 int AVI_ticks = 0;
 int buttonValue = 1;
 
@@ -91,83 +91,98 @@ void init_buttons_pio() {
 	return;
 }
 
-int switch_check() {
-	/* Checks State of Switches and Updates Modes Accordingly
-	 * Also updates LCD */
-	// Open LCD to Write
+int lcd_update(int mode) {
 	FILE *lcd;
 	lcd = fopen(LCD_NAME, "w");
-	// Clear LCD
 	fprintf(lcd, "%c%s", ESC, CLEAR_LCD_STRING);
-	// Get Switch Values
+	if (mode == 2)
+	fprintf(lcd, "Current Mode: 2\n Using UART");
+	else if (mode == 1)
+	fprintf(lcd, "Current Mode: 1\n Using Buttons");
+	fclose(lcd);
+	return 0;
+}
+
+int switch_check() {
+	/* Checks State of Switches and Updates Modes Accordingly */
 	int mode = IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_BASE);
 	//Mode Check
 	if (mode & (1 << 0)) {
-		fprintf(lcd, "Current Mode: 2\n Using UART\n", mode);
 		currentMode = 2;
 	} else {
-		fprintf(lcd, "Current Mode: 1\n Using Buttons\n", mode);
 		currentMode = 1;
 	}
-	// Close LCD
-	fclose(LCD_NAME);
 	//Reset SCCharts if mode has changed
 	if (currentMode != prevMode) {
+		lcd_update(currentMode);
 		prevMode = currentMode;
 		reset();
 	}
 	return 0;
 }
 
+
 int timer_handler() {
 		//Check Start
 		if (AVI_start) {
+			printf("AVI_start");
 			//Start AVI timer interrupt
 			alt_alarm_start(&AVI_timer, AVI_VALUE, AVI_timer_ISR, NULL);
 		}
 		if (PVARP_start) {
+			printf("PVARP_start");
 			//Start PVARP timer interrupt
 			alt_alarm_start(&PVARP_timer, PVARP_VALUE, PVARP_timer_ISR, NULL);
 		}
 		if (AEI_start) {
+			printf("AEI_start");
 			//Start AEI timer interrupt
 			alt_alarm_start(&AEI_timer, AEI_VALUE, AEI_timer_ISR, NULL);
 		}
 		if (VRP_start) {
+			printf("VRP_start");
 			//Start PVARP timer interrupt
 			alt_alarm_start(&VRP_timer, VRP_VALUE, VRP_timer_ISR, NULL);
 		}
 		if (LRI_start) {
+			printf("LRI_start");
 			//Start LRI timer interrupt
 			alt_alarm_start(&LRI_timer, LRI_VALUE, LRI_timer_ISR, NULL);
 		}
 		if (URI_start) {
+			printf("URI_start");
 			//Start PVARP timer interrupt
 			alt_alarm_start(&URI_timer, URI_VALUE, URI_timer_ISR, NULL);
 		}
 
 		//Check Stop
 		if (AVI_stop) {
+			printf("AVI_Stop");
 			//Start AVI timer interrupt
 			alt_alarm_stop(&AVI_timer);
 		}
 		if (PVARP_stop) {
+			printf("PVARP_Stop");
 			//Start PVARP timer interrupt
 			alt_alarm_stop(&PVARP_timer);
 		}
 		if (AEI_stop) {
+			printf("AEI_Stop");
 			//Start AEI timer interrupt
 			alt_alarm_stop(&AEI_timer);
 		}
 		if (VRP_stop) {
+			printf("VRP_Stop");
 			//Start VRP timer interrupt
 			alt_alarm_stop(&VRP_timer);
 		}
 		if (LRI_stop) {
+			printf("LRI_Stop");
 			//Start LRI timer interrupt
 			alt_alarm_stop(&LRI_timer);
 		}
 		if (URI_stop) {
+			printf("URI_Stop");
 			//Start PVARP timer interrupt
 			alt_alarm_stop(&URI_timer);
 		}
@@ -176,22 +191,31 @@ int timer_handler() {
 
 int setLights() {
 	//Thinking about adding a timer to handle extending LED on duration
-	if (APace)
+	if (APace) {
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, APACE_LED);
-	else if (VPace)
+		printf("APace");
+	}
+	else if (VPace) {
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, VPACE_LED);
-	else if (ASense)
+		printf("VPace");
+	}
+	else if (ASense) {
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, ASENSE_LED);
-	else if (VSense)
+		printf("ASense");
+	}
+	else if (VSense) {
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, VSENSE_LED);
+		printf("VSense");
+	}
 	else
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0);
 	return 0;
 }
 
 int main(void) {
+	tick();
 	//Initialize with AVI timer
-	AVI_ex = 1;
+	init_var = 1;
 	while (1) {
 		tick();
 		switch_check();
